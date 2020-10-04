cfb4b86b4a745f34f8278c84f41de2267cda0514
Adi Yerembessov <ayerembessov24@gmail.com>
2020-10-04 12:30:26 +0900

    half-way of changing sort

--- array_test.py
+++ array_test.py
@@ -1,15 +1,54 @@
 import numpy as np
-import random
-
-x = [1, 2, 3, 4]
-
-p_size = 4
-half_p_size = 2
-
-half = x[:2]
-
-print(half)
+import numpy as np
 
 
+
+y = np.array([1, 4, 2, 3])
+
+z = np.argsort(y, kind="mergesort")
+
+x = np.array([10, 40, 20, 30])
+
+x = x[z]
+y = y[z]
+
+print(x)
+
+print(y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# best tour
+
+
+# f = open("best_tour.txt", "r")
+
+# best_tour = np.zeros(dim)
+
+# index = 0
+# for city in f:
+# 	best_tour[index] = int(city)
+# 	index = index + 1
+
+# distance = 0
+# for i in range(dim - 1):
+# 	distance = distance + graph[int(best_tour[i]) - 1][int(best_tour[i + 1]) - 1]
+
+# distance = distance + graph[int(best_tour[dim - 1]) - 1][int(best_tour[0]) - 1]
+
+# print(distance)
+
+# f.close()
 
 
--- tsp_solver.py
+++ tsp_solver.py
@@ -8,6 +8,7 @@ optlist, args = getopt.getopt(args, 'p:f:')
 
 textfile = args[0]
 
+#parse the file
 f = open(textfile, "r")
 
 parsing_nodes = False 
@@ -31,15 +32,28 @@ for line in f:
 
 f.close()
 
-# population size and number of iterations
-#change later
+
+# set population size and number of iterations
+
+#default values
 p_size = dim
-iterations = 3 
+evaluations = dim * 100
+
+for par in optlist:
+	if par[0] == "-f":
+		evaluations = int(par[1])
+	if par[0] == "-p":
+		p_size = int(par[1])
+
+iterations = 0
+
+iterations = evaluations // p_size 
 
 
-graph = np.zeros((dim, dim))
 
 #calculate edge weights
+graph = np.zeros((dim, dim))
+
 for i in range(dim):
 	graph[i][i] = 0
 
@@ -49,9 +63,8 @@ for i in range(1, dim):
 		graph[j][i] = graph[i][j] 
 
 
-print("graph is done")
 
-#construction of MST
+#construct MST
 class Node:
 	def __init__(self, vertex):
 		self.vertex = vertex
@@ -95,11 +108,10 @@ for i in range(dim - 2):
 			node.parent = min_vertex
 			nodes[min_vertex].children.append(j)
 
-print("mst is done")
-
-# dfs to obtain TSP tour
 
 
+
+#non recursive dfs
 def dfs_non_rec():
 	discovered = np.zeros(dim)
 	tour = np.zeros(dim)
@@ -119,7 +131,7 @@ def dfs_non_rec():
 
 	return tour
 
-
+#recursive dfs
 def dfs_rec():
 	global iteration, discovered, tour
 	discovered = np.zeros(dim)
@@ -152,7 +164,7 @@ def crossover_and_mutation(parent1, parent2):
 	start = random.randint(0, dim - 1)
 	end = random.randint(start + 1, dim)
 
-	child = []
+	child = np.zeros(dim)
 
 	sublist = parent1[start:end]
 
@@ -163,21 +175,21 @@ def crossover_and_mutation(parent1, parent2):
 		vertex = parent2[parent2_index] 
 		
 		if vertex not in sublist:
-			child.append(vertex)
+			child[child_index] = vertex
 			child_index = child_index + 1
 
 		parent2_index = parent2_index + 1
 
-	print(child)
-	print(sublist)
-	child = child + sublist
+	for i in range(start, end):
+		child[i] = sublist[i - start]
+
 	child_index = child_index + end - start 
 
 	while child_index < dim:
 		vertex = parent2[parent2_index]
 
 		if vertex not in sublist:
-			child.append(vertex)
+			child[child_index] = vertex
 			child_index = child_index + 1
 
 		parent2_index = parent2_index + 1
@@ -190,8 +202,9 @@ def crossover_and_mutation(parent1, parent2):
 	return child
 
 def sort_population(population, distances, size):
-	sorted_indices = np.argsort(distances)
-	sorted_distances = np.sort(distances)
+	sorted_indices = np.argsort(distances, kind="mergesort") 
+	sorted_distances = distances[sorted_indices]
+	sorted_population = population[sorted_indices]
 
 	sorted_population = []
 
@@ -224,7 +237,7 @@ half_p_size = p_size // 2
 quart_p_size = half_p_size // 2 # number of solutions replaces each iteration
 parents_remaining = p_size - quart_p_size # number of parents remaining after each iteration
 	
-
+#if any of these are zero, there's no reason to go into crossover and mutation
 if number_of_branching_nodes == 0 or p_size == 0 or iterations == 0:
 	tour = dfs_rec()
 
@@ -263,7 +276,8 @@ for i in range(half_p_size):
 	
 population, distances = sort_population(population, distances, p_size)
 
-
+#create children and put some into the new population
+#repeat as many times as was set in parameters
 for i in range(iterations - 1):
 	children = []
 	children_distances = np.zeros(half_p_size)
@@ -274,69 +288,23 @@ for i in range(iterations - 1):
 		children.append(child)
 		children_distances[j] = calc_distance(child)
 
-	children, children_distances = sort_population(children, children_distances, children_replaces)
+	children, children_distances = sort_population(children, children_distances, quart_p_size)
 
 	population = population[:parents_remaining] + children
-	distances = distances[:parents_remaining] + children_distances
+	distances = np.concatenate((distances[:parents_remaining], children_distances))
 
 	population, distances = sort_population(population, distances, p_size)
 
 
 
-
+#output
 print(distances[0])
 
+f = open("solution.csv", "w")
 
-	
-
-
-
-# for i in range(p_size - 1):
-# 	if sorted_distances[i] > sorted_distances[i + 1]:
-# 		print(i)
+for i in range(dim):
+	f.write(str(int(population[0][i] + 1)) + "\n")
 
 
+f.close()
 
-
-# for i in range(p_size):
-
-	
-
-
-
-
-
-
-
-
-# print(calc_distance(tour))
-
-# f = open("solution.csv", "w")
-
-# for i in range(dim):
-# 	f.write(str(int(tour[i] + 1)) + "\n")
-
-
-# f.close()
-
-# best tour
-
-
-# f = open("best_tour.txt", "r")
-
-# best_tour = np.zeros(dim)
-
-# index = 0
-# for city in f:
-# 	best_tour[index] = int(city)
-# 	index = index + 1
-
-# distance = 0
-# for i in range(dim - 1):
-# 	distance = distance + graph[int(best_tour[i]) - 1][int(best_tour[i + 1]) - 1]
-
-# distance = distance + graph[int(best_tour[dim - 1]) - 1][int(best_tour[0]) - 1]
-
-# print(distance)
-
-# f.close()
